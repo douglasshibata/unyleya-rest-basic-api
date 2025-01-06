b75f76750ca8b9b35ec1e69d835baf2abd511d9f
Douglas Shibata <douglas.shibata11@gmail.com>
2025-01-06 20:22:54 -0300

    fix: removing logs"

--- src/main/java/br/edu/unyleya/pos/graduacao/dev/jakarta/repository/EmpresaRepository.java
+++ src/main/java/br/edu/unyleya/pos/graduacao/dev/jakarta/repository/EmpresaRepository.java
@@ -1,27 +1,25 @@
 package br.edu.unyleya.pos.graduacao.dev.jakarta.repository;
 
 import br.edu.unyleya.pos.graduacao.dev.jakarta.model.Empresa;
+import br.edu.unyleya.pos.graduacao.dev.jakarta.model.Endereco;
 import jakarta.ejb.Stateless;
 import jakarta.inject.Inject;
 
 @Stateless
-public class EmpresaRepository extends  BaseRepository<Empresa> {
+public class EmpresaRepository extends BaseRepository<Empresa> {
 
     @Inject
     private EnderecoRepository endRepository;
 
     @Override
     public Empresa save(Empresa entity) {
+        Endereco endereco = entity.getEndereco();
         entity = super.save(entity);
-        System.out.println(entity.toString());
-        System.out.println(entity.getEndereco());
-        if(entity.getEndereco() != null){
-            entity.getEndereco().setEmpresa(entity);
-            entity.setEndereco(endRepository.save(entity.getEndereco()));
-        }
-        System.out.println(entity.toString());
+        if (entity.getEndereco() != null) {
+            endereco.setEmpresa(entity);
+            entity.setEndereco(endRepository.save(endereco));
+        }        
         return entity;
     }
-   
 
 }
--- src/main/resources/META-INF/initial-data.sql
+++ src/main/resources/META-INF/initial-data.sql
@@ -1,1 +1,2 @@
 INSERT INTO empresa (nome) VALUES ('Empresa-A');
+INSERT INTO endereco (bairro,empresa_id) VALUES ('Bairro-A',1);
